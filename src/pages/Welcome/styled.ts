a4f17ea24018c3c11fb6f00a6e4e0b20419f6b1f
Helmut Schreier <6533040+HSchreier@users.noreply.github.com>
2024-07-29 20:28:14 +0200

    working on footer

--- src/pages/Welcome/styled.ts
+++ src/pages/Welcome/styled.ts
@@ -1,5 +1,5 @@
 import { styled } from '@mui/material/styles';
 
-const Image = styled('img')({ maxWidth: '80%', maxHeight: '100%', padding: '0', zIndex: '1' });
+const Image = styled('img')({ maxWidth: '75%', maxHeight: '100%', padding: '0', zIndex: '1' });
 
 export { Image };
--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -1,11 +1,26 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
 import useTheme from '@/store/theme';
+import { repository } from '@/config';
 
 function Footer() {
   const [theme] = useTheme();
   return (
-    <Box sx={{ flexGrow: 1 }} data-pw={`theme-${theme}`}>
-      <center>Copyright ...</center>
+    <Box
+      sx={{
+        flexGrow: 1,
+        zIndex: 1,
+        bottom: 0,
+        width: '100%',
+        color: '#000000',
+        position: 'fixed',
+      }}
+      data-pw={`theme-${theme}`}
+    >
+      <IconButton color="info" size="large" component="a" href={repository} target="_blank">
+        <GitHubIcon />
+      </IconButton>
     </Box>
   );
 }

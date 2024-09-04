b8089c4e363b841d7ac4c59e752ab2b7ba25bbaf
Helmut Schreier <6533040+HSchreier@users.noreply.github.com>
2024-09-04 18:43:28 +0200

    playn arround with clouds
    displacement map didn't work

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -3,6 +3,9 @@ import { FullSizeCenteredFlexBox } from '@/components/styled';
 import useOrientation from '@/hooks/useOrientation';
 import { GlobalStyles } from '@mui/material';
 import CloudBackground from '@/components/Bg/CloudBackground';
+import ObjModel from '@/components/Bg/ObjModel';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 function Welcome() {
   const isPortrait = useOrientation();
@@ -11,7 +14,23 @@ function Welcome() {
       <Meta title="Welcome" />
       <GlobalStyles styles={{}} />
       <FullSizeCenteredFlexBox flexDirection={isPortrait ? 'column' : 'row'}>
-        <CloudBackground />
+        <Canvas shadows camera={{ position: [0, 1.5, 8], fov: 35 }}>
+          {/* eslint-disable-next-line */}
+          <ambientLight intensity={0.5} />
+          {/* eslint-disable-next-line */}
+          <pointLight position={[10, 10, 10]} />
+
+          {/* Rotate the entire scene by 90 degrees to the left around the Y-axis */}
+          <group>
+            {/* Clouds Background */}
+            <CloudBackground />
+
+            {/* 3D Model */}
+            <ObjModel url="./logo3.obj" scale={0.005} />
+          </group>
+
+          <OrbitControls />
+        </Canvas>
       </FullSizeCenteredFlexBox>
     </>
   );

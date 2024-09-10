922f4f939be8483e004d4e3556db694eb4ceca58
Helmut Schreier <6533040+HSchreier@users.noreply.github.com>
2024-09-11 01:42:35 +0200

    hacked the watter effect

---
+++ dist/assets/index-CwA_Zl25.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-sI3zEing.js";import{M as e}from"./Meta-BCSRbRMg.js";import{F as t,T as a}from"./App-BrxddhJb.js";import"./index-9ekQf-c-.js";import"./index.esm-DjEbyMou.js";function p(){return r.jsxs(r.Fragment,{children:[r.jsx(e,{title:"page 2"}),r.jsx(t,{children:r.jsx(a,{variant:"h3",children:"Page 2"})})]})}export{p as default};
--- dist/assets/index-DIFdbLlI.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-xNnwmBma.js";import{M as e}from"./Meta-BkUuERFO.js";import{F as t,T as a}from"./App-DiWZrIKD.js";import"./index-BWv8SOSl.js";import"./index.esm-Dn9cDnZ6.js";function p(){return r.jsxs(r.Fragment,{children:[r.jsx(e,{title:"page 1"}),r.jsx(t,{children:r.jsx(a,{variant:"h3",children:"Page 1"})})]})}export{p as default};

3aac1169397fd1a401a12ee5ec2eb09184ebe456
Helmut Schreier <6533040+HSchreier@users.noreply.github.com>
2024-09-10 23:49:25 +0200

    playn arround added ocean
    ocean style

---
+++ src/components/WaterSky.tsx
@@ -0,0 +1,94 @@
+// src/components/WaterSky.tsx
+import { useRef, useEffect, useState } from 'react';
+import * as THREE from 'three';
+import { useThree, extend, useFrame } from '@react-three/fiber';
+import { Water } from 'three/examples/jsm/objects/Water';
+import { Sky } from 'three/examples/jsm/objects/Sky';
+import { useTexture } from '@react-three/drei';
+import MainLogoObj from './MainLogoObj';
+
+extend({ Water, Sky });
+
+const WaterSky = () => {
+  const waterRef = useRef<THREE.Water>(null);
+  const meshRef = useRef<THREE.Mesh>(null);
+  const { scene, gl } = useThree();
+  const uri = './logo3.obj';
+
+  const [sun, setSun] = useState(new THREE.Vector3());
+
+  const waterNormals = useTexture('/textures/waternormals.jpg');
+  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+
+  useEffect(() => {
+    const sky = new Sky();
+    sky.scale.setScalar(10000);
+    scene.add(sky);
+
+    const skyUniforms = (sky.material as THREE.ShaderMaterial).uniforms;
+    skyUniforms['turbidity'].value = 10;
+    skyUniforms['rayleigh'].value = 2;
+    skyUniforms['mieCoefficient'].value = 0.005;
+    skyUniforms['mieDirectionalG'].value = 0.8;
+
+    const pmremGenerator = new THREE.PMREMGenerator(gl);
+    const parameters = { elevation: 2, azimuth: 180 };
+
+    const updateSun = () => {
+      const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+      const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+      const newSun = new THREE.Vector3().setFromSphericalCoords(1, phi, theta);
+      setSun(newSun);
+
+      sky.material.uniforms['sunPosition'].value.copy(newSun);
+      waterRef.current!.material.uniforms['sunDirection'].value.copy(newSun).normalize();
+
+      const renderTarget = pmremGenerator.fromScene(sky);
+      scene.environment = renderTarget.texture;
+    };
+
+    updateSun();
+  }, [scene, gl]);
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    if (waterRef.current) {
+      waterRef.current.material.uniforms['time'].value += 1.0 / 60.0;
+    }
+
+    if (meshRef.current) {
+      meshRef.current.position.y = Math.sin(time) * 20 + 5;
+      meshRef.current.rotation.x = time * 0.5;
+      meshRef.current.rotation.z = time * 0.51;
+    }
+  });
+
+  // ts-expect-error unkown
+  return (
+    // ts-expect-error unkown
+    <>
+      {/* @ts-expect-error unkown*/}
+      <water
+        ref={waterRef} // eslint-disable-next-line
+        args={[
+          new THREE.PlaneGeometry(10000, 10000),
+          {
+            textureWidth: 512,
+            textureHeight: 512,
+            waterNormals: waterNormals,
+            sunDirection: sun,
+            sunColor: 0xffffff,
+            waterColor: 0x001e0f,
+            distortionScale: 3.7,
+            fog: scene.fog !== undefined,
+          },
+        ]} // eslint-disable-next-line
+        rotation-x={-Math.PI / 2}
+      />
+      <MainLogoObj url={uri} scale={0.1} />
+    </>
+  );
+};
+
+export default WaterSky;
--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,42 +1,16 @@
 import Meta from '@/components/Meta';
 import { FullSizeCenteredFlexBox } from '@/components/styled';
 import useOrientation from '@/hooks/useOrientation';
-import CloudBackground from '@/components/Bg/CloudBackground';
-import MainLogoObj from './MainLogoObj';
-import Birds from '@/components/Bg/Birds'; // Import Birds component
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import OceanScene from './scenes/OceanScene';
 
 function Welcome() {
   const isPortrait = useOrientation();
-  const r = (Math.random() + 1).toString(36).substring(7);
-  const uri = './logo3.obj?v=' + r.toString();
 
   return (
     <>
       <Meta title="Welcome" />
       <FullSizeCenteredFlexBox flexDirection={isPortrait ? 'column' : 'row'} height="100vh">
-        <Canvas
-          gl={{ preserveDrawingBuffer: true }}
-          onCreated={({ gl }) => {
-            gl.setClearColor('lightblue'); // Set background color here
-          }}
-          shadows
-          camera={{ position: [0, 1.5, 12], fov: 35 }}
-        >
-          {/* eslint-disable-next-line */}
-          <ambientLight intensity={1} />
-          {/* eslint-disable-next-line */}
-          <pointLight position={[10, 10, 10]} />
-
-          <group>
-            <CloudBackground />
-            <MainLogoObj url={uri} scale={0.005} />
-            <Birds /> {/* Render birds */}
-          </group>
-
-          <OrbitControls />
-        </Canvas>
+        <OceanScene />
       </FullSizeCenteredFlexBox>
     </>
   );
